12d3ffcc947c74e5f1397e8c156c43a3eabe3e07
ambid17 <12086352+ambid17@users.noreply.github.com>
2020-05-20 19:18:23 -0500

    added final Entity Framework code

--- DnD-api/DnD-api/Migrations/20200521000139_inital.Designer.cs
+++ DnD-api/DnD-api/Migrations/20200521000139_inital.Designer.cs
@@ -3,14 +3,16 @@ using DnD_api.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DnD_api.Migrations
 {
     [DbContext(typeof(CharacterContext))]
-    partial class CharacterContextModelSnapshot : ModelSnapshot
+    [Migration("20200521000139_inital")]
+    partial class inital
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DnD-api/DnD-api/Models/Character.cs
+++ DnD-api/DnD-api/Models/Character.cs
@@ -1,11 +1,36 @@
-﻿namespace DnD_api.Models
+﻿using Microsoft.CodeAnalysis;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DnD_api.Models
 {
     public class Character
     {
+        public enum Classes
+        {
+            Barbarian, Bard, Cleric, Druid, Rogue
+        }
+
+        public enum Races
+        {
+            Dwarf, Elf, Tiefling, Human
+        }
+
         public long CharacterId { get; set; }
 
+        [MaxLength(100)]
         public string Name { get; set; }
-        
+
+        public Races Race { get; set; }
+
+        public Classes Class { get; set; }
+
+        [Range(1,20)]
+        public int Level { get; set; }
+
+        public string Background { get; set; }
+
         public virtual CharacterStats CharacterStats { get; set; }
     }
 }
